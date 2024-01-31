4595fdaff7af968c5483d69b2bcb67ba144fcc7b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-01-31 16:32:24 +0545

    Additional cHANGES

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
+from langchain.agents import initialize_agent, AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+from langchain.tools import tool
+import requests
 import os
-import openai
 import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-openai.api_key = os.environ['GOOGLE_API_KEY']
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.llms import OpenAI
+api_key = os.environ['OPENAI_API_KEY']
+serpapi=os.environ['SERPAPI_API_KEY']
 st.title("Blog Generator")
+llm1 = OpenAI()
 
 user_input = st.text_input("Enter a topic to create a blog on:")
-no_words = st.number_input("Enter the number of words you want in your blog:", min_value=100, max_value=15000)
-user_style = st.text_input("Enter the style of your blog:")
+no_words = st.number_input("Enter the number of words you want in your blog:")
+tool_names = ["serpapi"]
+tools = load_tools(tool_names)
 
-
+agent = initialize_agent(tools, llm1, agent="zero-shot-react-description", verbose=True)
+new=agent.run(user_input)
 prompt=f"""
-Your task is to create a new blog on the topic of {user_input} in about {no_words} words. The blog should be written to match the style of {user_style}.\
-You should create a title for the blog as well.
+Your task is to create a new blog on the topic of {user_input} in about {no_words} words. \
+DO NOT MAKE THE OUTPUT HAVE LESS WORDS THAN {no_words}.\    
+You can use any relevant information or quotes that come up during this process.\
+You should create a title for the blog as well. If {user_input} is empty, do not generate blogs.\
+Incorporate content of {new} in the response as well.\
+Add the contents from  as you seem fit.\
+DO NOT HALLUCINATE. 
 """
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
-
-
 #UI using Streamlit:
-query=prompt
-if query:
-    result = llm.invoke(prompt)
-    st.text("Chat History:")
-    st.write(result.content)
-    st.write("Number of words:", len(result.content.split()))
+if len(user_input)==0 or no_words<150:
+    st.write("Input text to generate blog! or make sure that the number of words is more than 150 or equal to it")
+else :
+    result = llm1.invoke(prompt)
+    st.text("Blog:")
+    st.write(result)
+    # st.write("Number of words:", len(result.content.split()))
+    with open("Chat_history_Project.txt", "a") as file:
+        file.write(f"User: {user_input}\n")
+        file.write(f"AI: {result}\n\n")
