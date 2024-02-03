dffb592c7543bfe66f863e3fa91f7ba97303c5ac
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-03 07:18:12 +0545

    Changes to updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -13,12 +13,12 @@ api_key = os.environ.get('OPENAI_API_KEY')  # Use get to avoid KeyError if the k
 
 # Initializing Language Model (LLM) and search tool
 llm1 = OpenAI()  # Choosing LLM
-search = DuckDuckGoSearchRun()
+search = DuckDuckGoSearchRun()#Search Tool 
 
 # Retrieve input data from the request
 def get_input_data():
     try:
-        request_data = json.loads(input())  # Assuming the input is in JSON format
+        request_data = json.loads(input())  # Assuming the input is in JSON format(If it's not, than our code will turn to JSON)
         topic = request_data['topic']
         return topic
     except Exception as e:
@@ -31,31 +31,28 @@ def generate_blog(title):
         search_result = search.run(title)
         # Defining prompt
         prompt = f"""
-        You can use any relevant information or quotes that come up during this process.\
-        You should create a title for the blog as well.\
-        Incorporate content from {search_result} in the response as well.\
-        Your response should be in JSON format.\
-        Add the contents from as you seem fit.\
-        DO NOT HALLUCINATE.
-        If your response ends abruptly than display upto the last sentence.\
-            For example If your response is like this:
-            '........ And so that's how Computers work. Now'
-        Your response should be '........ And so that's how Computers work.\
-        ##Topic:{title}
-        """
+        Generate a blog post with the following topic: "{title}". \
+        The blog post should be concise and contain approximately 500 words. \
+        Incorporate relevant information about {title} and provide insights into its applications.
+        Use the content from {search_result} in your response as well. \
+        Avoid abrupt endings and ensure that the response is complete.\
+        Your output should be in the format shown below:\
+        'Blog Title':{title}"\n"
+        'Your generated content'
 
-        # Generating blog using the Language Model
-        result = llm1.invoke(prompt)
-    
+        """
+        result = llm1.invoke(prompt, max_tokens=750)
+
         # Documenting chat history in a .txt file
         with open("Chat_history_Project.txt", "a") as file:
             file.write(f"User: {title}\n")
             file.write(f"Response: {result}\n\n")
-
+        #Documenting resposne history in a .txt file
         with open("responses.txt", "a") as file:
             file.write(f"Blog: {result}\n")
 
-        return result
+        return result#Returning the blog
+    #Exception Handling
     except Exception as e:
         print(f"Error: {e}")
         return str(e)
